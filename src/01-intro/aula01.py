e45440ae7efb8b1157ba328baa2a7618b00fa215
sthecss <sthephanny_css@hotmail.com>
2025-12-14 20:57:19 -0300

    commit inicial

---
+++ src/01-intro/aula01.py
@@ -0,0 +1,13 @@
+""" aula 01"""
+
+print("olá mundo!")
+
+
+def somar(numero_01, numero_02):
+    """ somar - soma dois nuemros"""
+    return numero_01 + numero_02
+
+
+SOMA = somar(10.3, 2.5)
+
+print(SOMA)
