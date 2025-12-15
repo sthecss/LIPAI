51fec2373a14217c01fbb89e40365e617f9d74ec
sthecss <sthephanny_css@hotmail.com>
2025-12-14 21:18:40 -0300

    beleza

---
+++ src/01-intro/aula03.py
@@ -0,0 +1,29 @@
+""" Aula 03 - Comentários """
+
+# Comentário de uma unica linha
+
+'''
+Isso é um comentário de
+multiplas linhas
+'''
+
+
+n1 = 10
+n2 = 30
+n3 = 78
+
+# ctrl + / e o código acima fica:
+# n1 = 10
+# n2 = 30
+# n3 = 78
+
+# ctrl + / de novo e tira o # de comentário:
+# n1 = 10
+n2 = 30
+# n3 = 78
+
+# É de bom tom a intenção do código já estar exposto no algoritmo, então comentários em excesso podem ser sinal de alerta.
+
+# Comentário é bom para documentação e afins, em projetos finais não é tão bom.
+
+# Foque em algoritmos bons o suficiente para comentários não serem necessários.
